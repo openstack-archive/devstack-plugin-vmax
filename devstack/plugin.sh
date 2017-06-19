98b1e8dbc3af98a454bd9937aa2cf2961149a9f8
Unana Okpoyo <unana.okpoyo@emc.com>
2017-06-19 10:08:41 +0100

    Create newton branch of devstack plugin for vmax

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -6,7 +6,7 @@
 function configure_port_groups {
     local be_name=$1
     echo "<PortGroups>" >> \
-        ${CINDER_CONF_DIR}/cinder_dell_emc_config_$be_name.xml
+        ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
     vmax_temp="${be_name}_PortGroup"
     dell_emc_portGroups=0
     for i in ${!VMAX*}; do
@@ -22,10 +22,10 @@ function configure_port_groups {
     for (( m=1 ; m<=dell_emc_portGroups ; m++ )) ; do
         vmax_temp="${be_name}_PortGroup${m}"
         echo "<PortGroup>${!vmax_temp}</PortGroup>" >> \
-        ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
+        ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
     done
     echo "</PortGroups>" >> \
-        ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
+        ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
 }
 
 function configure_single_pool {
@@ -36,12 +36,12 @@ function configure_single_pool {
         vmax_temp="${be_name}_${val}"
         if [  -n "${!vmax_temp}" ]; then
             echo "<${val}>${!vmax_temp}</${val}>" >> \
-            ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
+            ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
         fi
     done
     vmax_temp="${be_name}_SLO"
     if [  -n "${!vmax_temp}" ]; then
-        echo "<ServiceLevel>${!vmax_temp}</ServiceLevel>" >> \
+        echo "<SLO>${!vmax_temp}</SLO>" >> \
         ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
     fi
     configure_port_groups ${be_name}
@@ -49,32 +49,31 @@ function configure_single_pool {
 
 function configure_cinder_backend_vmax {
     local be_name=$1
-    local emc_multi=${be_name%%_*}
     iniset ${CINDER_CONF} ${be_name} volume_backend_name ${be_name}
     storage_proto="${be_name}_StorageProtocol"
-    vmax_directory="cinder.volume.drivers.dell_emc.vmax."
+    vmax_directory="cinder.volume.drivers.emc.emc_vmax_"
     if [[ "${!storage_proto}" == "iSCSI" ]]; then
         iniset ${CINDER_CONF} ${be_name} volume_driver \
-        "${vmax_directory}iscsi.VMAXISCSIDriver"
+        "${vmax_directory}iscsi.EMCVMAXISCSIDriver"
     fi
     if [ "${!storage_proto}" = "FC" ]; then
         iniset ${CINDER_CONF} ${be_name} volume_driver \
-        "${vmax_directory}fc.VMAXFCDriver"
+        "${vmax_directory}fc.EMCVMAXFCDriver"
     fi
     iniset ${CINDER_CONF} ${be_name} driver_use_ssl "True"
-    iniset ${CINDER_CONF} ${be_name} cinder_dell_emc_config_file \
-    "$CINDER_CONF_DIR/cinder_dell_emc_config_$be_name.xml"
+    iniset ${CINDER_CONF} ${be_name} cinder_emc_config_file \
+    "$CINDER_CONF_DIR/cinder_emc_config_$be_name.xml"
 
-    touch ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
+    touch ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
     echo "<?xml version='1.0' encoding='UTF-8'?>" > \
-    ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
-    echo "<EMC>" >> ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
+    ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
+    echo "<EMC>" >> ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
 
     configure_single_pool ${be_name}
 
-    echo "</EMC>" >> ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml
+    echo "</EMC>" >> ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml
     if [ ! -f "$CINDER_CONF_DIR/cinder_emc_config.xml" ]; then
-        ln -s ${CINDER_CONF_DIR}/cinder_dell_emc_config_${be_name}.xml \
+        ln -s ${CINDER_CONF_DIR}/cinder_emc_config_${be_name}.xml \
             ${CINDER_CONF_DIR}/cinder_emc_config.xml
     fi
 }
